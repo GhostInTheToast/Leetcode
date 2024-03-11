4c5ecdafab0290b682dc0feb6b99c2d08a96e7f1
Bilal Mahmood <bmood1@gmail.com>
2024-03-11 03:25:11 -0500

    Create 3Sum.py
    3Sum solved in python 3.

---
+++ 3Sum.py
@@ -0,0 +1,18 @@
+class 3Sum:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        nums.sort()
+        for i in range(len(nums)-2):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:    
+                if nums[i] + nums[j] + nums[k] == 0:
+                    ans.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+                
+        return ans

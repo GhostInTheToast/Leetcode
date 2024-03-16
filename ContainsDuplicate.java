a664e6ee0950f94924b86fc1dc1e72043a769363
Bilal Mahmood <bmood1@gmail.com>
2024-03-16 15:51:18 -0700

    Update ContainsDuplicate.java
    Different iteration method, less code.

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -2,14 +2,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
-
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> hs = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (hs.contains(nums[i])) return true;
-            hs.add(nums[i]);
+        Set<Integer> set = new HashSet<Integer>();
+        for(int num : nums) {
+            if (set.contains(num))
+                return true;
+            set.add(num);
         }
         return false;
     }
-
 }

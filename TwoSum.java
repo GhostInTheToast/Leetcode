3f58204bf5439e127d24e96b8f4202ea4d56435b
Bilal Mahmood <bmood1@gmail.com>
2024-03-16 17:17:12 -0700

    Update TwoSum.java
    Cleaner and more concise code.

--- TwoSum.java
+++ TwoSum.java
@@ -1,18 +1,11 @@
-import java.util.HashMap;
-
 public class TwoSum {
-
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        int need;
+        Map<Integer, Integer> hmap = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            need = target - nums[i];
-            if (hm.containsKey(need))
-                return new int[] {i,hm.get(need)};
-            else
-                hm.put(nums[i], i);
+            if (hmap.containsKey(target - nums[i])) 
+                return new int[] {i, hmap.get(target - nums[i])};
+            hmap.put(nums[i], i);
         }
         return new int[] {0,0};
     }
-
 }
